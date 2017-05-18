40e656970033150203bba6ca84f5e2fbebb3ee7d
Lukasz Wojno <l.wojno@imagho.pl>
2017-05-19 00:25:02 +0200

    First dev working version.

--- spface.py
+++ spface.py
@@ -10,10 +10,10 @@ faceCascade = cv2.CascadeClassifier(cascPath)
 
 video_capture = cv2.VideoCapture(0)
 video_capture.set(3,1280)
-video_capture.set(4,1024)
+video_capture.set(4,720)
 
 while True:
-    time.sleep(2)
+    # time.sleep(2)
     # Capture frame-by-frame
     ret, frame = video_capture.read()
 
@@ -21,7 +21,7 @@ while True:
 
     faces = faceCascade.detectMultiScale(
         gray,
-        scaleFactor=1.2,
+        scaleFactor=1.1,
         minNeighbors=5,
         minSize=(30, 30),
         flags=cv2.cv.CV_HAAR_SCALE_IMAGE
@@ -30,7 +30,7 @@ while True:
     # Cut the faces into separate files with timestamp
     for (x, y, w, h) in faces:
         # Draw rectangle around the faces
-        # cv2.rectangle(frame, (x-50, y-50), (x+w+50, y+h+50),0,0)
+        cv2.rectangle(frame, (x-50, y-50), (x+w+50, y+h+50),(0,255,0),0)
         head = frame[y-50:y+h+50, x-50:x+w+50]
         filename="head{}".format(int(time.time()))
         # cv2.imshow(filename, head)
